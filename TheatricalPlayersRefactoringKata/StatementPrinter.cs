f6e388590a84419ecb1921b1ad67841d6d496ab0
Mark Ollenburger <mollenbu77@gmail.com>
2022-12-11 20:48:00 -0700

    Refactor print function

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -6,47 +6,86 @@ namespace TheatricalPlayersRefactoringKata
 {
     public class StatementPrinter
     {
+        private CultureInfo cultureInfo;
+        private int volumeCredits = 0;
+        private string formattedResult;
+
         public string Print(Invoice invoice, Dictionary<string, Play> plays)
         {
-            var totalAmount = 0;
-            var volumeCredits = 0;
-            var result = string.Format("Statement for {0}\n", invoice.Customer);
-            CultureInfo cultureInfo = new CultureInfo("en-US");
+            cultureInfo = new CultureInfo("en-US");
+            formattedResult = string.Format("Statement for {0}\n", invoice.Customer);
+            var invoiceTotalAmount = getInvoiceTotalAmountAndVolumeCredits(invoice, plays);
+            formattedResult += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(invoiceTotalAmount / 100));
+            formattedResult += String.Format("You earned {0} credits\n", volumeCredits);
+            return formattedResult;
+        }
 
-            foreach(var perf in invoice.Performances) 
+        private int getInvoiceTotalAmountAndVolumeCredits(Invoice invoice, Dictionary<string, Play> plays)
+        {
+            var invoiceTotalAmount = 0;
+            foreach (var performance in invoice.Performances)
             {
-                var play = plays[perf.PlayID];
-                var thisAmount = 0;
-                switch (play.Type) 
-                {
-                    case "tragedy":
-                        thisAmount = 40000;
-                        if (perf.Audience > 30) {
-                            thisAmount += 1000 * (perf.Audience - 30);
-                        }
-                        break;
-                    case "comedy":
-                        thisAmount = 30000;
-                        if (perf.Audience > 20) {
-                            thisAmount += 10000 + 500 * (perf.Audience - 20);
-                        }
-                        thisAmount += 300 * perf.Audience;
-                        break;
-                    default:
-                        throw new Exception("unknown type: " + play.Type);
-                }
-                // add volume credits
-                volumeCredits += Math.Max(perf.Audience - 30, 0);
-                // add extra credit for every ten comedy attendees
-                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
-
-                // print line for this order
-                result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
-                totalAmount += thisAmount;
+                var play = plays[performance.PlayID];
+                var playAmountOwed = getPlayAmountOwed(performance.Audience, play);
+                volumeCredits += getAudienceVolumeCredits(performance.Audience, play);
+                formattedResult += getFormattedLineOrder(performance.Audience, play, playAmountOwed);
+                invoiceTotalAmount += playAmountOwed;
             }
-            result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
-            result += String.Format("You earned {0} credits\n", volumeCredits);
-            return result;
+            return invoiceTotalAmount;
+        }
+
+        private string getFormattedLineOrder(int audienceCount, Play play, int thisAmount)
+        {
+            return String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), audienceCount);
+        }
+
+        private static int getAudienceVolumeCredits(int audienceCount, Play play)
+        {
+            // add volume credits
+            var volumeCredits = Math.Max(audienceCount - 30, 0);
+            // add extra credit for every ten comedy attendees
+            if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)audienceCount / 5);
+            return volumeCredits;
+        }
+
+        private static int getPlayAmountOwed(int audienceCount, Play play)
+        {
+            int thisAmount = 0;
+            switch (play.Type)
+            {
+                case "tragedy":
+                    thisAmount = getTragedyAmountOwed(audienceCount);
+                    break;
+                case "comedy":
+                    thisAmount = getComedyAmountOwed(audienceCount);
+                    break;
+                default:
+                    throw new Exception("unknown type: " + play.Type);
+            }
+
+            return thisAmount;
+        }
+
+        private static int getComedyAmountOwed(int audienceCount)
+        {
+            int thisAmount = 30000;
+            if (audienceCount > 20)
+            {
+                thisAmount += 10000 + 500 * (audienceCount - 20);
+            }
+            thisAmount += 300 * audienceCount;
+            return thisAmount;
+        }
+
+        private static int getTragedyAmountOwed(int audienceCount)
+        {
+            int thisAmount = 40000;
+            if (audienceCount > 30)
+            {
+                thisAmount += 1000 * (audienceCount - 30);
+            }
+
+            return thisAmount;
         }
     }
 }
